c31a419b63131d471c55f28bbb162ddb8297afbc
Tiko34 <tixonkov@list.ru>
2024-10-30 00:58:49 +1000

    Добавлены все знаки зодиака

--- TGBotGoroskop.py
+++ TGBotGoroskop.py
@@ -51,6 +51,131 @@ def user_message(message):
 
 #Переход к функции taurus_days_selection для выбора дальнешего действия
 			bot.register_next_step_handler(message, taurus_days_selection)
+		case 'Овен':
+#создание клавиатуры для Овна
+			taurus = ReplyKeyboardMarkup(resize_keyboard=True)
+#Берет строки из списка DefaultButton и циклом делает каждую строку
+#отдельной кнопкой
+			for DB in DefaultButton:
+				taurus.add(KeyboardButton(str(DB)))
+			bot.send_message(message.chat.id, 'На какой день хотите получить гороскоп?', reply_markup=taurus)
+
+#Переход к функции taurus_days_selection для выбора дальнешего действия
+			bot.register_next_step_handler(message, aries_days_selection)
+		case 'Близнецы':
+#создание клавиатуры для Близнецов
+			taurus = ReplyKeyboardMarkup(resize_keyboard=True)
+#Берет строки из списка DefaultButton и циклом делает каждую строку
+#отдельной кнопкой
+			for DB in DefaultButton:
+				taurus.add(KeyboardButton(str(DB)))
+			bot.send_message(message.chat.id, 'На какой день хотите получить гороскоп?', reply_markup=taurus)
+
+#Переход к функции taurus_days_selection для выбора дальнешего действия
+			bot.register_next_step_handler(message, gemini_days_selection)
+		case 'Рак':
+#создание клавиатуры для Рака
+			taurus = ReplyKeyboardMarkup(resize_keyboard=True)
+#Берет строки из списка DefaultButton и циклом делает каждую строку
+#отдельной кнопкой
+			for DB in DefaultButton:
+				taurus.add(KeyboardButton(str(DB)))
+			bot.send_message(message.chat.id, 'На какой день хотите получить гороскоп?', reply_markup=taurus)
+
+#Переход к функции taurus_days_selection для выбора дальнешего действия
+			bot.register_next_step_handler(message, cancer_days_selection)
+		case 'Лев':
+#создание клавиатуры для Рака
+			taurus = ReplyKeyboardMarkup(resize_keyboard=True)
+#Берет строки из списка DefaultButton и циклом делает каждую строку
+#отдельной кнопкой
+			for DB in DefaultButton:
+				taurus.add(KeyboardButton(str(DB)))
+			bot.send_message(message.chat.id, 'На какой день хотите получить гороскоп?', reply_markup=taurus)
+
+#Переход к функции taurus_days_selection для выбора дальнешего действия
+			bot.register_next_step_handler(message, leo_days_selection)
+		case 'Дева':
+#создание клавиатуры для Рака
+			taurus = ReplyKeyboardMarkup(resize_keyboard=True)
+#Берет строки из списка DefaultButton и циклом делает каждую строку
+#отдельной кнопкой
+			for DB in DefaultButton:
+				taurus.add(KeyboardButton(str(DB)))
+			bot.send_message(message.chat.id, 'На какой день хотите получить гороскоп?', reply_markup=taurus)
+
+#Переход к функции taurus_days_selection для выбора дальнешего действия
+			bot.register_next_step_handler(message, virgo_days_selection)
+		case 'Весы':
+#создание клавиатуры для Рака
+			taurus = ReplyKeyboardMarkup(resize_keyboard=True)
+#Берет строки из списка DefaultButton и циклом делает каждую строку
+#отдельной кнопкой
+			for DB in DefaultButton:
+				taurus.add(KeyboardButton(str(DB)))
+			bot.send_message(message.chat.id, 'На какой день хотите получить гороскоп?', reply_markup=taurus)
+
+#Переход к функции taurus_days_selection для выбора дальнешего действия
+			bot.register_next_step_handler(message, libra_days_selection)
+		case 'Скорпион':
+#создание клавиатуры для Рака
+			taurus = ReplyKeyboardMarkup(resize_keyboard=True)
+#Берет строки из списка DefaultButton и циклом делает каждую строку
+#отдельной кнопкой
+			for DB in DefaultButton:
+				taurus.add(KeyboardButton(str(DB)))
+			bot.send_message(message.chat.id, 'На какой день хотите получить гороскоп?', reply_markup=taurus)
+
+#Переход к функции taurus_days_selection для выбора дальнешего действия
+			bot.register_next_step_handler(message, scorpio_days_selection)
+		case 'Стрелец':
+#создание клавиатуры для Рака
+			taurus = ReplyKeyboardMarkup(resize_keyboard=True)
+#Берет строки из списка DefaultButton и циклом делает каждую строку
+#отдельной кнопкой
+			for DB in DefaultButton:
+				taurus.add(KeyboardButton(str(DB)))
+			bot.send_message(message.chat.id, 'На какой день хотите получить гороскоп?', reply_markup=taurus)
+
+#Переход к функции taurus_days_selection для выбора дальнешего действия
+			bot.register_next_step_handler(message, sagittarius_days_selection)
+		case 'Козерог':
+#создание клавиатуры для Рака
+			taurus = ReplyKeyboardMarkup(resize_keyboard=True)
+#Берет строки из списка DefaultButton и циклом делает каждую строку
+#отдельной кнопкой
+			for DB in DefaultButton:
+				taurus.add(KeyboardButton(str(DB)))
+			bot.send_message(message.chat.id, 'На какой день хотите получить гороскоп?', reply_markup=taurus)
+
+#Переход к функции taurus_days_selection для выбора дальнешего действия
+			bot.register_next_step_handler(message, capricorn_days_selection)
+		case 'Водолей':
+#создание клавиатуры для Рака
+			taurus = ReplyKeyboardMarkup(resize_keyboard=True)
+#Берет строки из списка DefaultButton и циклом делает каждую строку
+#отдельной кнопкой
+			for DB in DefaultButton:
+				taurus.add(KeyboardButton(str(DB)))
+			bot.send_message(message.chat.id, 'На какой день хотите получить гороскоп?', reply_markup=taurus)
+
+#Переход к функции taurus_days_selection для выбора дальнешего действия
+			bot.register_next_step_handler(message, aquarius_days_selection)
+		case 'Рыбы':
+#создание клавиатуры для Рака
+			taurus = ReplyKeyboardMarkup(resize_keyboard=True)
+#Берет строки из списка DefaultButton и циклом делает каждую строку
+#отдельной кнопкой
+			for DB in DefaultButton:
+				taurus.add(KeyboardButton(str(DB)))
+			bot.send_message(message.chat.id, 'На какой день хотите получить гороскоп?', reply_markup=taurus)
+
+#Переход к функции taurus_days_selection для выбора дальнешего действия
+			bot.register_next_step_handler(message, pisces_days_selection)
+
+
+
+
 
 
 def taurus_days_selection(message):
@@ -70,17 +195,193 @@ def taurus_days_selection(message):
 			temp = bs.findAll('p')
 			for data in temp:
 				bot.send_message(message.chat.id,data)		
+def aries_days_selection(message):
 
+	match message.text:
+		case 'Завтра':
+			url='https://horo.mail.ru/prediction/aries/tomorrow/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,'lxml')
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)			
+		case 'Сегодня':
+			url='https://horo.mail.ru/prediction/aries/today/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,"lxml")
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)		
+def gemini_days_selection(message):
 
+	match message.text:
+		case 'Завтра':
+			url='https://horo.mail.ru/prediction/gemini/tomorrow/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,'lxml')
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)			
+		case 'Сегодня':
+			url='https://horo.mail.ru/prediction/gemini/today/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,"lxml")
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)		
+def cancer_days_selection(message):
 
+	match message.text:
+		case 'Завтра':
+			url='https://horo.mail.ru/prediction/cancer/tomorrow/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,'lxml')
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)			
+		case 'Сегодня':
+			url='https://horo.mail.ru/prediction/cancer/today/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,"lxml")
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)		
+def leo_days_selection(message):
 
+	match message.text:
+		case 'Завтра':
+			url='https://horo.mail.ru/prediction/leo/tomorrow/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,'lxml')
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)			
+		case 'Сегодня':
+			url='https://horo.mail.ru/prediction/leo/today/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,"lxml")
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)	
+def virgo_days_selection(message):
 
-			
+	match message.text:
+		case 'Завтра':
+			url='https://horo.mail.ru/prediction/virgo/tomorrow/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,'lxml')
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)			
+		case 'Сегодня':
+			url='https://horo.mail.ru/prediction/virgo/today/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,"lxml")
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)	
+def libra_days_selection(message):
 
+	match message.text:
+		case 'Завтра':
+			url='https://horo.mail.ru/prediction/libra/tomorrow/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,'lxml')
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)			
+		case 'Сегодня':
+			url='https://horo.mail.ru/prediction/libra/today/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,"lxml")
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)				
+def scorpio_days_selection(message):
 
+	match message.text:
+		case 'Завтра':
+			url='https://horo.mail.ru/prediction/scorpio/tomorrow/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,'lxml')
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)			
+		case 'Сегодня':
+			url='https://horo.mail.ru/prediction/scorpio/today/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,"lxml")
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)	
+def sagittarius_days_selection(message):
 
+	match message.text:
+		case 'Завтра':
+			url='https://horo.mail.ru/prediction/sagittarius/tomorrow/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,'lxml')
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)			
+		case 'Сегодня':
+			url='https://horo.mail.ru/prediction/sagittarius/today/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,"lxml")
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)	
+def capricorn_days_selection(message):
 
+	match message.text:
+		case 'Завтра':
+			url='https://horo.mail.ru/prediction/capricorn/tomorrow/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,'lxml')
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)			
+		case 'Сегодня':
+			url='https://horo.mail.ru/prediction/capricorn/today/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,"lxml")
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)	
+def aquarius_days_selection(message):
 
+	match message.text:
+		case 'Завтра':
+			url='https://horo.mail.ru/prediction/aquarius/tomorrow/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,'lxml')
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)			
+		case 'Сегодня':
+			url='https://horo.mail.ru/prediction/aquarius/today/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,"lxml")
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)	
+def pisces_days_selection(message):
+
+	match message.text:
+		case 'Завтра':
+			url='https://horo.mail.ru/prediction/pisces/tomorrow/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,'lxml')
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)			
+		case 'Сегодня':
+			url='https://horo.mail.ru/prediction/pisces/today/'
+			response = requests.get(url)
+			bs = BeautifulSoup(response.text,"lxml")
+			temp = bs.findAll('p')
+			for data in temp:
+				bot.send_message(message.chat.id,data)	
 
 
 
