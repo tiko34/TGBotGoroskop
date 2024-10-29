a4dfc971f85c9c8841992984cd9c7d3af877969b
Tiko34 <tixonkov@list.ru>
2024-10-29 21:44:50 +1000

    Вывод кнопками всех знаков зодиака

---
+++ BotToken.py
@@ -0,0 +1,1 @@
+Token = '7879188155:AAE4imoe7NHYyLgPk36E1mMm_cZ8Fxhw_zU'
--- TGBotGoroskop.py
+++ TGBotGoroskop.py
@@ -1,1 +1,35 @@
+## -*- coding: utf-8 -*-
+import types
+import telebot # type: ignore
+#Получение токена из файла BotToken.py
+from BotToken import Token
+#Получение списка знаков зодиака из файла ZodiacSigns.py
+from ZodiacSigns import ZodiacSigns
+#Получение класса для Reply клавиатуры
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton # type: ignore
+#Получение ботом токена
+bot = telebot.TeleBot(Token)
+#Обработка команды /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	#создание клавиатуры
+	main = ReplyKeyboardMarkup(resize_keyboard=True)
+	#Берет строки из списка ZodiacSigns и циклом делает каждую строку
+	#отдельной кнопкой
+	for fd in ZodiacSigns:
+		main.add(KeyboardButton(str(fd)))
+	#выдача клавиатуры пользователю и вывод сообщения
+	bot.reply_to(message, 'Выберите знак зодиака', reply_markup=main)
 
+
+
+
+
+
+
+
+
+
+
+
+bot.infinity_polling()
